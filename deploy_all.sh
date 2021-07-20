5d97eb605b6af3b118ebbef07e585240e456202c
Steven de Oliveira <de.oliveira.steven@gmail.com>
2021-07-20 11:32:38 +0200

    Updating scripts for latest ft version

--- deploy_all.sh
+++ deploy_all.sh
@@ -8,4 +8,4 @@ bash deploy.sh CrystalVault &&
 bash deploy.sh Bid &&
 bash deploy.sh AuctionRoot '{"bid_address":"%{account:address:Bid}"}' &&
 
-ft multisig -a user1 --transfer 10 --to AuctionRoot init '{"english_auction":"%{account:address:EnglishAuction}", "english_reverse_auction":"%{account:address:EnglishReverseAuction}" ,"dutch_auction":"%{account:address:DutchAuction}" ,"dutch_reverse_auction":"%{account:address:DutchReverseAuction}", "bid_builder":"%{account:address:BidBuilder}"}'
+ft multisig transfer 10 --from user1 --to AuctionRoot init '{"english_auction":"%{account:address:EnglishAuction}", "english_reverse_auction":"%{account:address:EnglishReverseAuction}" ,"dutch_auction":"%{account:address:DutchAuction}" ,"dutch_reverse_auction":"%{account:address:DutchReverseAuction}", "bid_builder":"%{account:address:BidBuilder}"}'
